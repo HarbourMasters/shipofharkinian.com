cfcaa84a57ffc93b50bcee76f48abb13c161d7bb
PurpleHato <linkvssangoku.jr@gmail.com>
2022-11-02 19:09:22 +0100

    Add: localePath to i18next

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr'],
+    localePath: path.resolve('./public/locales'),
   },
   fallbackLng: 'en',
   keySeparator: false,
