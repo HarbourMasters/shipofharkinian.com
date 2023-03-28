e4d67eeba08cfd464c4582477531e18121644e27
aMannus <mannusmenting@gmail.com>
2023-03-28 16:50:48 +0200

    Added another line

--- src/pages/changelog.tsx
+++ src/pages/changelog.tsx
@@ -38,6 +38,7 @@ const Home: NextPage = () => {
                     <ul>
                       <li>Added bee movie script to potential Gossip Stone hints.</li>
                       <li>Removed Mido from the game because nobody liked him anyway.</li>
+                      <li>Added a second README just in case people don't read the first one.</li>
                     </ul>
                     <h4>Bugfixes</h4>
                     <ul>
