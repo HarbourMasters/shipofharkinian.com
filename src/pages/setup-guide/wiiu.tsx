1fdc2beaa203f63b6a119858d7df47fb80935385
Malkierian <malkierian@gmail.com>
2023-08-08 09:24:39 -0700

    Add Switch and Wii U guides.

--- src/pages/setup-guide/wiiu.tsx
+++ src/pages/setup-guide/wiiu.tsx
@@ -8,14 +8,14 @@ import { PageFooter } from '../../components/PageFooter';
 
 const Home: NextPage = () => {
   const { t } = useTranslation();
-  const pageName = "Windows Setup Guide";
+  const pageName = "Wii U Setup Guide";
 
   return (
-    <>
+      <>
 
-      <PageHeader pageName={pageName} />
+        <PageHeader pageName={pageName} />
 
-      <main className={pageName}>
+        <main className={pageName}>
           <section id="content">
             <h1 className="pageTitle">{pageName}</h1>
             <div className="container">
@@ -27,8 +27,12 @@ const Home: NextPage = () => {
                       <a href="#step-1">Step 1: Downloading the Ship of Harkinian</a>
                     </h2>
                     <p className="guideNoteBold">Note: This is the code that will interact with the Assets required in Step 2. This does NOT provide you with any Nintendo Licensed Assets…</p>
-                    <p><a href="https://discord.com/channels/808039310850130000/955966600891224064">Click here</a> to go to the <b>#download</b> channel in the Discord, where you will find the links for <b>all current and historical versions</b> of Ship of Harkinian. 
-						Find the <b>highest</b> numbered version and select the link that says <b>Windows</b>.
+                    <p className="guideNoteBold">Note: You will need a PC to complete this process.</p>
+
+                    <h3 style={"text-align: center;"}>On your PC</h3>
+                    <p><a href="https://discord.com/channels/808039310850130000/955966600891224064">Click here</a> to go to the <b>#downloads</b> channel in the Discord, where you will find the links for <b>all current and historical versions</b> of Ship of Harkinian.
+                      Find the <b>highest</b> numbered version and select the link that says indicates the <b>PC platform</b> you will be using. Also download the link that says <b>Wii U</b>, you will need this later.<br />
+                      <span className={"guideNoteBold"}>Note: The PC platform version is required to generate the oot.otr that will be used later on the Switch.</span>
                     </p>
                     <p><b>Extract the Zip file</b> into a Folder on your <b>Local Hard Drive</b> for later.<br />
                       <span className="guideNoteBold">Note: Do NOT just open the zip and run SoH from there, things will break.</span><br />
@@ -38,31 +42,23 @@ const Home: NextPage = () => {
                     <h2 id="step-2">
                       <a href="#step-2">Step 2: Obtaining a ROM (Legally)</a>
                     </h2>
-                    <p>Follow the steps outlined in <Link href="dump-guide">ROM Dumping Guide</Link> to <span className="guideNoteBold">legally</span> obtain a <b>usable ROM</b> for the Ship of Harkinian.</p>
-                    <p>The ReadMe file <b>included with the download</b> mentioned in <a href="#step-1">Step 1</a> lists out the <b>CRC/SHA1 Hash</b> and a website to verify your version. <span className="guideNoteBold">Incorrect versions are not supported</span> and you will see an error or the game will not load similar to the below screenshot:</p>
+                    <p>Follow Step 2: Obtaining a ROM (Legally) and Step 3: Extracting the Assets and Launching Ship of Harkinian on the PC Platform you selected above.</p>
                     <image href="images/image5.png"></image>
 
                     <h2 id="step-3">
                       <a href="#step-3">Step 3: Extracting the Assets and Launching Ship of Harkinian</a>
                     </h2>
-                    <p>Assuming you correctly followed the instructions in Steps 1 and 2 you are now ready for the last process.</p>
-                    <p>Open the Folder you extracted from Step 1 and launch the soh.exe file.
-                      <span className={"guideNoteBold"}>Note: You can ignore any AntiVirus or Windows SmartScreen notifications, these are false positives due to these programs having too little information about Ship of Harkinian.</span>
+                    <p>Once the oot.otr file has been generated, you no longer need the <b>PC Platform</b> build. The remainder of the steps here will be using the <b>Wii U files you downloaded earlier in <a href={"#step-1"}>Step 1</a></b>.</p>
+                    <p>Create the following <b>folder structure</b> on your <b>SD card</b>:<br />
+                      <span style="margin-left: 100px;">sdcard</span><br />
+                      <span style="margin-left: 100px;">└──switch</span><br />
+                      <span style="margin-left: 130px;">└──soh</span><br />
+                      <span style="margin-left: 160px;">└──oot.otr</span><br />
+                      <span style="margin-left: 160px;">└──soh.nro</span><br />
+                      <span style="margin-left: 160px;">└──soh.otr</span>
                     </p>
-                    <p>Once <b>soh.exe</b> has been opened for the first time you will be greeted with a popup window. Select Yes at this prompt:<br />
-                      <image href={"images/image4.png"}></image>
-                    </p>
-                    <p>If your ROM is <b>not located in the same folder as the SoH.exe</b> you will have one additional prompt. Select Yes at this prompt too:<br />
-                      <image href={"images/image3.png"}></image><br />
-                      Locate the ROM that you <b>dumped in Step 2</b> and double click it to begin the Asset Extraction process.
-                    </p>
-                    <p>This Asset Extraction process will happen silently in the background.<br />
-                      <span className={"guideNote"}>Note: You can safely ignore any errors displayed during this process, as long as the console window closes and you see the following prompt:</span><br />
-                      <image href={"images/image2.png"}></image><br />
-                      If you have a Master Quest ROM you wish to create an oot-mq.otr from, select Yes. Otherwise, select No.
-                    </p>
-                    <p>Once you have completed the extraction process the Game will automatically launch.</p>
-                    <p className={"guideNote"}>Note: If you are performing this operation for use with a console platform, please continue on to step 3 for either <Link href={"switch#step-3"}>Nintendo Switch</Link> or <Link href={"wiiu#step-3"}>Wii U</Link> guides.</p>
+                    <h3 style={"text-align: center;"}>On your Wii U</h3>
+                    <p>Insert the SD Card and launch the game via <b>Atmosphere’s Game+R</b> launcher method.</p>
                     <p><b>Congratulations, you can now play Ocarina of Time on the Ship of Harkinian.</b></p>
 
                   </div>
@@ -72,15 +68,15 @@ const Home: NextPage = () => {
           </section>
         </main>
 
-      <PageFooter />
+        <PageFooter />
 
-    </>
+      </>
   );
 };
 
 export const getServerSideProps = async({ locale }: { locale: string }) => ({
   props: {
-  ...(await serverSideTranslations(locale, ['common']))
+    ...(await serverSideTranslations(locale, ['common']))
   }
 });
 
