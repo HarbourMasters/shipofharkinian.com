33ae45a632bfd2a79952861555114ffdcf53021b
Malkierian <malkierian@gmail.com>
2023-08-08 13:50:19 -0700

    Add ROM dumping guide.

--- src/pages/setup-guide/dump-guide.tsx
+++ src/pages/setup-guide/dump-guide.tsx
@@ -6,16 +6,18 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { PageHeader } from '../../components/PageHeader';
 import { PageFooter } from '../../components/PageFooter';
 
+import image1 from './images/image13.png';
+
 const Home: NextPage = () => {
   const { t } = useTranslation();
-  const pageName = "Windows Setup Guide";
+  const pageName = "Update Guide";
 
   return (
-    <>
+      <>
 
-      <PageHeader pageName={pageName} />
+        <PageHeader pageName={pageName} />
 
-      <main className={pageName}>
+        <main className={pageName}>
           <section id="content">
             <h1 className="pageTitle">{pageName}</h1>
             <div className="container">
@@ -23,47 +25,102 @@ const Home: NextPage = () => {
                 <div className="textBackground">
                   <div className="textArea">
 
-                    <h2 id="step-1">
-                      <a href="#step-1">Step 1: Downloading the Ship of Harkinian</a>
-                    </h2>
-                    <p className="guideNoteBold">Note: This is the code that will interact with the Assets required in Step 2. This does NOT provide you with any Nintendo Licensed Assets…</p>
-                    <p><a href="https://discord.com/channels/808039310850130000/955966600891224064">Click here</a> to go to the <b>#download</b> channel in the Discord, where you will find the links for <b>all current and historical versions</b> of Ship of Harkinian. 
-						Find the <b>highest</b> numbered version and select the link that says <b>Windows</b>.
+                    <p>The below steps will explain how to <b>obtain a ROM</b> from your <b>legally</b> sourced, official game disc.<br />
+                      <span className="guideNoteBold">Reminder: Obtaining the Official Game Disc is your responsibility.</span>
                     </p>
-                    <p><b>Extract the Zip file</b> into a Folder on your <b>Local Hard Drive</b> for later.<br />
-                      <span className="guideNoteBold">Note: Do NOT just open the zip and run SoH from there, things will break.</span><br />
-                      <span className="guideNoteBold">Note: Do NOT extract this to C:\Windows, C:\Program Files, or a OneDrive/Google Drive location, things will break.</span>
+                    <h3>Requirements:</h3>
+                    <p><ul>
+                      <li>Softmodded <Link href={"https://wii.guide/"}>Wii</Link> or <Link href={"https://wiiu.hacks.guide/"}>Wii U</Link></li>
+                      <li>PAL/European Gamecube Disc</li>
+                      <li>Windows PC (it could work on Linux/macOS, but is currently untested)</li>
+                      <li><Link href={"https://dolphin-emu.org/"}>Dolphin</Link></li>
+                      <li><Link href={"https://mh-nexus.de/en/hxd/"}>HxD</Link> or equivalent hex editor</li>
+                      <li>Ship of Harkinian ( <Link href={"https://github.com/HarbourMasters/Shipwright"}>GitHub</Link> | <Link href={"https://discord.com/invite/shipofharkinian"}>Discord</Link> )</li>
+                    </ul>
                     </p>
-
-                    <h2 id="step-2">
-                      <a href="#step-2">Step 2: Obtaining a ROM (Legally)</a>
-                    </h2>
-                    <p>Follow the steps outlined in <Link href="dump-guide">ROM Dumping Guide</Link> to <span className="guideNoteBold">legally</span> obtain a <b>usable ROM</b> for the Ship of Harkinian.</p>
-                    <p>The ReadMe file <b>included with the download</b> mentioned in <a href="#step-1">Step 1</a> lists out the <b>CRC/SHA1 Hash</b> and a website to verify your version. <span className="guideNoteBold">Incorrect versions are not supported</span> and you will see an error or the game will not load similar to the below screenshot:</p>
-                    <image href="images/image5.png"></image>
-
-                    <h2 id="step-3">
-                      <a href="#step-3">Step 3: Extracting the Assets and Launching Ship of Harkinian</a>
-                    </h2>
-                    <p>Assuming you correctly followed the instructions in Steps 1 and 2 you are now ready for the last process.</p>
-                    <p>Open the Folder you extracted from Step 1 and launch the soh.exe file.
-                      <span className={"guideNoteBold"}>Note: You can ignore any AntiVirus or Windows SmartScreen notifications, these are false positives due to these programs having too little information about Ship of Harkinian.</span>
+                    <h3>Procedure</h3>
+                    <p>
+                      <ol type={"1"}>
+                        <li>Dump your game disc using your <b>modded Wii or Wii U</b>. Transfer the dump to your PC.</li>
+                        <ol type={"a"}>
+                          <li><Link href={"https://wii.guide/dump-games.html"}>Dumping Wii/Gamecube Games</Link> @ Wii Guide</li>
+                          <li><Link href={"http://wiki.dolphin-emu.org/index.php?title=Ripping_Games"}>Ripping Games</Link> @ Dolphin Wiki</li>
+                        </ol>
+                        <li>Use <b>Dolphin</b> to extract the <b>file system contents</b> of your disc dump.</li>
+                        <ol type={"a"}>
+                          <li><Link href={"http://wiki.vg-resource.com/Dolphin"}>ISO Filesystem Browsing</Link> @ VG Resource Wiki</li>
+                        </ol>
+                        <li>Open the relevant <b>.tgc file</b> in HxD using the information in the table below:</li>
+                        <table id={"dumpTable"}>
+                          <thead>
+                          <tr>
+                            <th>Disc</th>
+                            <th>Game</th>
+                            <th>Filename</th>
+                          </tr>
+                          </thead>
+                          <tbody>
+                          <tr>
+                            <td>Ocarina of Time & Master Quest</td>
+                            <td>Ocarina of Time & Master Quest</td>
+                            <td>files/zlj_f.tgc</td>
+                          </tr>
+                          <tr>
+                            <td>Zelda Collector's Edition</td>
+                            <td>Ocarina of Time</td>
+                            <td>files/tgc/zelda_PAL_093003.tgc</td>
+                          </tr>
+                          </tbody>
+                        </table>
+                        <li>Open the "Select block" window (Ctrl + E). In the <b>Start-offset</b> field, enter the relevant <b>starting offset</b> from the table below:</li>
+                        <table id={"dumpTable"}>
+                          <thead>
+                          <tr>
+                            <th>Disc</th>
+                            <th>Game</th>
+                            <th>Starting Offset</th>
+                          </tr>
+                          </thead>
+                          <tbody>
+                          <tr>
+                            <td rowSpan={2}>Ocarina of Time & Master Quest</td>
+                            <td>Ocarina of Time</td>
+                            <td>1C3AA240</td>
+                          </tr>
+                          <tr>
+                            <td>Master Quest</td>
+                            <td>1E69CB38</td>
+                          </tr>
+                          <tr>
+                            <td>Zelda Collector's Edition</td>
+                            <td>Ocarina of Time</td>
+                            <td>1C669FC0</td>
+                          </tr>
+                          <tr>
+                            <td rowSpan={2}>AU OoT & Master Quest</td>
+                            <td>Ocarina of Time</td>
+                            <td>1E69EEF4</td>
+                          </tr>
+                          <tr>
+                            <td>Master Quest</td>
+                            <td>1C3AC5FC</td>
+                          </tr>
+                          </tbody>
+                        </table>
+                        <li>Ensure the <b>Length</b> and <b>Hex</b> options are checked, then enter <b>2000000 (2,000,000)</b> into the Length field.<br />
+                          <Image src={image1} alt={"dump guide select block"}/></li>
+                        <li>Once you've entered the correct <b>Length</b> and <b>Start-offset</b>, click "OK". The raw bytes of the ROM should be selected.</li>
+                        <li>Copy the selected data (Ctrl + C)</li>
+                        <ol type={"a"}>
+                          <li>Create a new blank file (Ctrl + N)</li>
+                          <li>Paste the data (Ctrl + N)<br />
+                            <span className={"guideNote"}>Note: If HxD displays a message about <b>changing the file size</b>, click "OK".</span></li>
+                        </ol>
+                        <li>Save the file (Ctrl + S) and place it in your Ship of Harkinian installation location, next to soh.exe.</li>
+                        <li>Validate your dumped ROM using <Link href={"http://romhacking.net/hash"}>Hasher-js</Link>.</li>
+                        <li>Proceed with <b>setting up Ship of Harkinian</b> as dictated in your <b>Platform Guide</b>.</li>
+                      </ol>
                     </p>
-                    <p>Once <b>soh.exe</b> has been opened for the first time you will be greeted with a popup window. Select Yes at this prompt:<br />
-                      <image href={"images/image4.png"}></image>
-                    </p>
-                    <p>If your ROM is <b>not located in the same folder as the SoH.exe</b> you will have one additional prompt. Select Yes at this prompt too:<br />
-                      <image href={"images/image3.png"}></image><br />
-                      Locate the ROM that you <b>dumped in Step 2</b> and double click it to begin the Asset Extraction process.
-                    </p>
-                    <p>This Asset Extraction process will happen silently in the background.<br />
-                      <span className={"guideNote"}>Note: You can safely ignore any errors displayed during this process, as long as the console window closes and you see the following prompt:</span><br />
-                      <image href={"images/image2.png"}></image><br />
-                      If you have a Master Quest ROM you wish to create an oot-mq.otr from, select Yes. Otherwise, select No.
-                    </p>
-                    <p>Once you have completed the extraction process the Game will automatically launch.</p>
-                    <p className={"guideNote"}>Note: If you are performing this operation for use with a console platform, please continue on to step 3 for either <Link href={"switch#step-3"}>Nintendo Switch</Link> or <Link href={"wiiu#step-3"}>Wii U</Link> guides.</p>
-                    <p><b>Congratulations, you can now play Ocarina of Time on the Ship of Harkinian.</b></p>
 
                   </div>
                 </div>
@@ -72,15 +129,15 @@ const Home: NextPage = () => {
           </section>
         </main>
 
-      <PageFooter />
+        <PageFooter />
 
-    </>
+      </>
   );
 };
 
 export const getServerSideProps = async({ locale }: { locale: string }) => ({
   props: {
-  ...(await serverSideTranslations(locale, ['common']))
+    ...(await serverSideTranslations(locale, ['common']))
   }
 });
 
