867c826970efbf454e44361694d765e71f1b99c7
Malkierian <malkierian@gmail.com>
2023-09-26 23:37:24 -0700

    I don't even know anymore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,8 @@ const Home: NextPage = () => {
               <span className="quote">{t('Proof that the unofficial option is sometimes the best option')}</span>
               <span className="author">- The Verge</span>
               <Link href="https://github.com/HarbourMasters/Shipwright/releases/latest">
-                <a target="_blank">
+                <a className="splashButton" target="_blank">
+                  <span>{t('Download on')}</span>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.6 31.8">
                     <title>GitHub</title>
                     <path d="M16.3,0C7.3,0,0,7.3,0,16.3c0,7.2,4.7,13.3,11.1,15.5c0.8,0.1,1.1-0.4,1.1-0.8c0-0.4,0-1.4,0-2.8  c-4.5,1-5.5-2.2-5.5-2.2c-0.7-1.9-1.8-2.4-1.8-2.4c-1.5-1,0.1-1,0.1-1c1.6,0.1,2.5,1.7,2.5,1.7c1.5,2.5,3.8,1.8,4.7,1.4  c0.1-1.1,0.6-1.8,1-2.2c-3.6-0.4-7.4-1.8-7.4-8.1c0-1.8,0.6-3.2,1.7-4.4C7.4,10.7,6.8,9,7.7,6.8c0,0,1.4-0.4,4.5,1.7  c1.3-0.4,2.7-0.5,4.1-0.5c1.4,0,2.8,0.2,4.1,0.5c3.1-2.1,4.5-1.7,4.5-1.7c0.9,2.2,0.3,3.9,0.2,4.3c1,1.1,1.7,2.6,1.7,4.4  c0,6.3-3.8,7.6-7.4,8c0.6,0.5,1.1,1.5,1.1,3c0,2.2,0,3.9,0,4.5c0,0.4,0.3,0.9,1.1,0.8c6.5-2.2,11.1-8.3,11.1-15.5  C32.6,7.3,25.3,0,16.3,0z"/>
