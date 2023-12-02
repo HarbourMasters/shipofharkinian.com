c852530e896755eb8227e8303659d3e5a7d1fa98
Malkierian <malkierian@gmail.com>
2023-12-01 23:32:40 -0700

    Change ROM verification link from Hasher to ship.equipment

--- src/pages/setup-guide/dump-guide.tsx
+++ src/pages/setup-guide/dump-guide.tsx
@@ -97,7 +97,7 @@ const Home: NextPage = () => {
                             <span className={"guideNote"}>Note: If HxD displays a message about <b>changing the file size</b>, click &quot;OK&quot;.</span></li>
                         </ol>
                         <li>Save the file (Ctrl + S) with a <b>.z64</b> extension, and place it in your Ship of Harkinian installation location, next to soh.exe.</li>
-                        <li>Validate your dumped ROM using <Link href={"http://romhacking.net/hash"}>Hasher-js</Link>.</li>
+                        <li>Validate your dumped ROM using <Link href={"https://ship.equipment"}>ship.equipement</Link>.</li>
                         <li>Proceed with <b>setting up Ship of Harkinian</b> as dictated in your <b>Platform Guide</b>.</li>
                       </ol>
                     </p>
