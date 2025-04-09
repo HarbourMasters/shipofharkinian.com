27a4a04a0833cbc25f0e59f0d77b096b10491dd0
Christopher Leggett <chris@leggett.dev>
2025-04-09 15:27:27 -0400

    Added a few more entries for recently merged stuff.

--- src/pages/changelog.tsx
+++ src/pages/changelog.tsx
@@ -35,6 +35,8 @@ const Home: NextPage = () => {
                     <li>Fixed the MQ Water Temple check totals.</li>
                     <li>Fixed an error in the Enemy Randomizer causing incorrect enemies to spawn when specific enemies were selected.</li>
                     <li>Shuffled Grass now respects the &ldquo;Chests of Agony&rdquo; option.</li>
+                    <li>Added a some more randomized currency names.</li>
+                    <li>Fixed the Autoscrolling in the Check Tracker when Only Show Available was selected.</li>
                   </ul>
                   <h3>NTSC</h3>
                   <p>NOTE: The following bugfixes require regenerating your OTR to be applied.</p>
